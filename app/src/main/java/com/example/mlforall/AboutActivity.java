e3383b44fa9bfa418323c517fd6646a52c1e19ba
Tomer Reiner <tomer3gaming2004@gmail.com>
2021-04-10 17:54:20 +0300

    Version 1.0.2 of My Machine Learning App

--- app/src/main/java/com/example/mlforall/AboutActivity.java
+++ app/src/main/java/com/example/mlforall/AboutActivity.java
@@ -9,11 +9,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -32,7 +29,7 @@ public class AboutActivity extends AppCompatActivity {
     private DatabaseHelper db;
     private MenuHelper menuHelper;
 
-    private TextView tvAbout;
+//    private TextView tvAbout;
 
     private Button btnHiddenFeature;
 
@@ -91,7 +88,7 @@ public class AboutActivity extends AppCompatActivity {
         username = db.getCurrentLoggedInUsername();
         menuHelper = new MenuHelper(AboutActivity.this, drawerLayout, navigationView, drawerToggle, actionBar, loginDialog, signUpDialog, db, username);
 
-        tvAbout = findViewById(R.id.tvAbout);
+//        tvAbout = findViewById(R.id.tvAbout);
 //        tvAbout.setMovementMethod(LinkMovementMethod.getInstance());
         btnHiddenFeature = findViewById(R.id.btnHiddenFeature);
     }
